b50de36a486fad226f5e8dc3d6e1a75fa95b1159
hahnhoffen <tim.hahnc@gmail.com>
2024-12-18 21:23:29 +0100

    staged

--- AIronChef.API/Program.cs
+++ AIronChef.API/Program.cs
@@ -1,4 +1,6 @@
 
+using System.Data.SqlClient;
+
 namespace AIronChef.API
 {
     public class Program
@@ -8,12 +10,19 @@ namespace AIronChef.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            string connectionString = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTION_STRING");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Azure SQL connection string is not set in environment variables.");
+            }
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddSingleton(new SqlConnection(connectionString));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
