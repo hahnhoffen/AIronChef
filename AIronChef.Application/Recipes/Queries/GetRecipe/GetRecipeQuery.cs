4d64fc2abc4977ac45970bff80b44827f15deff4
Emil Lindhult <emillindhult@gmail.com>
2025-01-08 22:25:51 +0100

    https://timhahnc.atlassian.net/browse/SCRUM-40

---
+++ AIronChef.Application/Recipes/Queries/GetRecipe/GetRecipeQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AIronChef.Application.Recipes.Queries.GetRecipe
+{
+    public class GetRecipeQuery
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "Id must be greater than zero")]
+        public int Id { get; set; }
+    }
+}
