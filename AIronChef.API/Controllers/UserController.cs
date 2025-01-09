9f40c5b01b6c23bc490db6299265416275c279b7
KungSimon <simon.naslund@hotmail.com>
2025-01-09 10:51:41 +0100

    Added NugetPackage ImediatR

--- AIronChef.API/Controllers/UserController.cs
+++ AIronChef.API/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
