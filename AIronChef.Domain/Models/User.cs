d9059d5bdaaf30ae9dbb2f98f889b27c5434d650
MirzaOrozco <mirza.orozco-alcantara@student.nbi-handelsakademin.se>
2025-01-09 22:43:10 +0100

    Added User entity class
    Create User Entity as described in https://timhahnc.atlassian.net/browse/SCRUM-10

---
+++ AIronChef.Domain/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AIronChef.Domain.Models
+{
+    public class User
+    {
+        // Primary key, auto-incremented by the database.
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        // User's full name.
+        [Required]
+        public required string Name { get; set; }
+
+        // User's email address.
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        // Hashed version of the user's password.
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
