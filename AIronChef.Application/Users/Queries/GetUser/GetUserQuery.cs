fbf32069bcd927a75f2ffb258700410b428790ac
Emil Lindhult <emillindhult@gmail.com>
2025-01-21 10:47:36 +0100

    https://timhahnc.atlassian.net/browse/SCRUM-32

---
+++ AIronChef.Application/Users/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,13 @@
+﻿using AIronChef.Application.Common.Helpers;
+using AIronChef.Domain.Models;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace AIronChef.Application.Users.Queries.GetUser
+{
+    public class GetUserQuery : IRequest<OperationResult<User>>
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "Id must be greater than zero.")]
+        public int Id { get; }
+    }
+}
