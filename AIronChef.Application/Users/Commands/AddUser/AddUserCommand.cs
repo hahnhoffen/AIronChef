7c40abce36c0ad35136e867530590e90b859a4b4
Mohammed Bekler <mohammed@kravia.ai>
2025-01-21 11:03:30 +0100

    Removed validations from AddUserCommand.cs

--- AIronChef.Application/Users/Commands/AddUser/AddUserCommand.cs
+++ AIronChef.Application/Users/Commands/AddUser/AddUserCommand.cs
@@ -4,24 +4,7 @@ namespace AIronChef.Application.Users.Commands.AddUser;
 
 public class AddUserCommand
 {
-    /// <summary>
-    /// Gets or sets the full name of the user.
-    /// </summary>
-    [Required(ErrorMessage = "Name is required")]
-    [StringLength(40, ErrorMessage = "Name cannot be longer than 40 characters")]
     public string Name { get; set; }
-    
-    /// <summary>
-    /// Gets or sets the email address of the user.
-    /// </summary>
-    [Required(ErrorMessage = "Email is required")]
-    [EmailAddress(ErrorMessage = "Email is invalid format.")]
     public string Email { get; set; }
-    
-    /// <summary>
-    /// Gets or sets the plain text password of the user.
-    /// </summary>
-    [Required(ErrorMessage = "Password is required")]
-    [StringLength(22, ErrorMessage = "Password cannot be longer than 22 characters")]
     public string Password { get; set; }
 }
