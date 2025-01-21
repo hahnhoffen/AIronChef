2a336c5a452825700b8b47481cb3a1499ad7e30d
KungSimon <simon.naslund@hotmail.com>
2025-01-21 10:51:23 +0100

    Added IUserReporitory

---
+++ AIronChef.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using AIronChef.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIronChef.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(int id);
+        Task<User> AddUserAsync(User user);
+        Task<User> UpdateUserAsync(User user);
+        Task<User> DeleteUserAsync(int id);
+        Task<bool> IsEmailUniqueAsync(string email);
+    }
+}
