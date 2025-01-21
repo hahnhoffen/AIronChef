f81d69a356802556fba89043842cb3c45f9ff7ff
Emil Lindhult <emillindhult@gmail.com>
2025-01-21 11:01:33 +0100

    Updated GetUserQuery

--- AIronChef.Application/Users/Queries/GetUser/GetUserQuery.cs
+++ AIronChef.Application/Users/Queries/GetUser/GetUserQuery.cs
@@ -1,13 +1,8 @@
 ﻿using AIronChef.Application.Common.Helpers;
 using AIronChef.Domain.Models;
 using MediatR;
-using System.ComponentModel.DataAnnotations;
 
 namespace AIronChef.Application.Users.Queries.GetUser
 {
-    public class GetUserQuery : IRequest<OperationResult<User>>
-    {
-        [Range(1, int.MaxValue, ErrorMessage = "Id must be greater than zero.")]
-        public int Id { get; }
-    }
+    public record GetUserQuery(int Id) : IRequest<OperationResult<User>>;
 }
