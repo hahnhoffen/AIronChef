6649e7d12d94f7ab28b0f87d64ebbb7a6784a72a
KungSimon <simon.naslund@hotmail.com>
2025-01-21 11:04:53 +0100

    Added IRecipeRepository

---
+++ AIronChef.Application/Interfaces/IRecipeRepository.cs
@@ -0,0 +1,18 @@
+﻿using AIronChef.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIronChef.Application.Interfaces
+{
+    public interface IRecipeRepository
+    {
+        Task<Recipe> GetRecipeByIdAsync(int id);
+        Task<IEnumerable<Recipe>> GetAllRecipesAsync();
+        Task AddRecipeAsync(Recipe recipe);
+        Task UpdateRecipeAsync(Recipe recipe);
+        Task DeleteRecipeAsync(int id);
+    }
+}
