81b1cc70a2aa7a75a9ee90688637ce980c68b00d
KungSimon <simon.naslund@hotmail.com>
2025-01-23 11:04:02 +0100

    Moved to the correct Interfaces folder

--- AIronChef.Domain/Interfaces/IRecipeRepository.cs
+++ AIronChef.Domain/Interfaces/IRecipeRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AIronChef.Application.Interfaces
+namespace AIronChef.Domain.Interfaces
 {
     public interface IRecipeRepository
     {
