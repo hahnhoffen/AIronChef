9608835b0b1ae754e6345d77248752a33ac18944
MirzaOrozco <mirza.orozco-alcantara@student.nbi-handelsakademin.se>
2025-01-23 15:16:29 +0100

    Add user command handler
    Added package "Konscious.Security.Cryptography.Argon2" Version="1.3.1" to get a Argon2 password hashing.
    Added PasswordHasher helper class to handle the password hashing
    Added the add user command handler that uses repository interface to add user
    Added password

---
+++ AIronChef.Application/Common/Helpers/PasswordHasher.cs
@@ -0,0 +1,40 @@
+﻿using AIronChef.Domain.Models;
+using Konscious.Security.Cryptography;
+using System.Text;
+
+namespace AIronChef.Application.Common.Helpers
+{
+    internal class PasswordHasher
+    {
+        public static string HashPassword(string password, string salt)
+        {
+            using (var hasher = new Argon2id(Encoding.UTF8.GetBytes(password)))
+            {
+                hasher.Salt = Convert.FromBase64String(salt);
+                hasher.DegreeOfParallelism = 8; // Number of threads
+                hasher.MemorySize = 65536; // 64 MB of memory
+                hasher.Iterations = 4; // Number of iterations
+                var hash = hasher.GetBytes(64); // Get 64-byte hash
+                return Convert.ToBase64String(hash); // Convert to string
+            }
+        }
+
+        public static string GenerateSalt()
+        {
+            int length = 16;
+            var salt = System.Security.Cryptography.RandomNumberGenerator.GetBytes(length);
+            return Convert.ToBase64String(salt); // Convert to string
+        }
+
+        public static bool VerifyPassword(string password, string storedHash, string storedSalt)
+        {
+            var passwordHash = HashPassword(password, storedSalt);
+            return passwordHash == storedHash;
+        }
+
+        public static bool VerifyPassword(string password, User user)
+        {
+            return VerifyPassword(password, user.PasswordHash, user.PasswordSalt);
+        }
+    }
+}
---
+++ AIronChef.Application/Users/Commands/AddUser/AddUserCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using AIronChef.Application.Common.Helpers;
+using AIronChef.Domain.Interfaces;
+using AIronChef.Domain.Models;
+using MediatR;
+
+namespace AIronChef.Application.Users.Commands.AddUser
+{
+    public class AddUserCommandHandler : IRequestHandler<AddUserCommand, OperationResult<User>>
+    {
+        private readonly IUserRepository _repository;
+
+        public AddUserCommandHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<OperationResult<User>> Handle(AddUserCommand command, CancellationToken cancellationToken)
+        {
+            // Check if title is valid
+            if (string.IsNullOrWhiteSpace(command.Name))
+            {
+                return OperationResult<User>.Failure("The User name can not be empty");
+            }
+            // Check if password is set
+            if (string.IsNullOrWhiteSpace(command.Password))
+            {
+                return OperationResult<User>.Failure("The Password can not be empty");
+            }
+            if (await _repository.IsEmailUniqueAsync(command.Email) == false)
+            {
+                return OperationResult<User>.Failure("The Email already exist");
+            }
+
+            try
+            {
+                var salt = PasswordHasher.GenerateSalt();
+                var passwordHash = PasswordHasher.HashPassword(command.Password, salt);
+                var userDTO = new User
+                {
+                    Name = command.Name,
+                    Email = command.Email,
+                    PasswordHash = passwordHash,
+                    PasswordSalt = salt
+                };
+                User user = await _repository.AddUserAsync(userDTO);
+                return OperationResult<User>.Successfull(user);
+            }
+            catch (Exception ex)
+            {
+                return OperationResult<User>.Failure(ex.Message);
+            }
+        }
+    }
+}
