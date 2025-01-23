0851caf7be7873fda466afcac1bcf3704da7eca6
KungSimon <simon.naslund@hotmail.com>
2025-01-23 14:53:40 +0100

    Added DeleteRecipeCommand

---
+++ AIronChef.Application/Recipes/Commands/DeleteRecipe/DeleteRecipeCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIronChef.Application.Recipes.Commands.DeleteRecipe
+{
+    public class DeleteRecipeCommand
+    {
+        public int Id { get; set; }
+
+        public bool IsValid()
+        {
+            return ValidationHelper.IsIdValid(Id);
+        }
+    }
+}
