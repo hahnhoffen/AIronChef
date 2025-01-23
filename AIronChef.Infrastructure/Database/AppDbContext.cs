b28a78b5fd9539032e62b4d6064bffcc69af44b6
Emil Lindhult <emillindhult@gmail.com>
2025-01-23 16:50:44 +0100

    Add User and Recipe to DbContext

--- AIronChef.Infrastructure/Database/AppDbContext.cs
+++ AIronChef.Infrastructure/Database/AppDbContext.cs
@@ -9,6 +9,8 @@ namespace AIronChef.Infrastructure.Database
         public DbSet<User> Users { get; set; }
         public DbSet<Recipe> Recipes { get; set; }
 
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
