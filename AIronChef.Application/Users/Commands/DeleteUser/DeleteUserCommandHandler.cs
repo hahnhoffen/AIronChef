514015badc117d2ae01534826473637e4172862a
KungSimon <simon.naslund@hotmail.com>
2025-01-31 13:29:57 +0100

    Added the DeleteUserHandlerTests and added ILoggingService

--- AIronChef.Application/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
+++ AIronChef.Application/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AIronChef.Application.Common.Helpers;
+using AIronChef.Application.Interfaces;
 using AIronChef.Domain.Interfaces;
 using AIronChef.Domain.Models;
 using MediatR;
@@ -8,10 +9,12 @@ namespace AIronChef.Application.Users.Commands.DeleteUser
     public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, OperationResult<User>>
     {
         private readonly IUserRepository _repository;
+        private readonly ILoggingService _loggingService;
 
-        public DeleteUserCommandHandler(IUserRepository repository)
+        public DeleteUserCommandHandler(IUserRepository repository, ILoggingService loggingService)
         {
             _repository = repository;
+            _loggingService = loggingService;
         }
 
         public async Task<OperationResult<User>> Handle(DeleteUserCommand command, CancellationToken cancellationToken)
@@ -19,8 +22,10 @@ namespace AIronChef.Application.Users.Commands.DeleteUser
             User user = await _repository.DeleteUserAsync(command.Id);
             if (user == null)
             {
+                _loggingService.LogWarning("Invalid id");
                 return OperationResult<User>.Failure("Invalid id");
             }
+            _loggingService.LogInfo("User deleted");
             return OperationResult<User>.Successfull(user);
         }
     }
---
+++ AIronChef.Tests/UnitTests/ApplicationTests/UsersTest/DeleteUserHandlerTests.cs
@@ -0,0 +1,83 @@
+﻿using AIronChef.Application.Interfaces;
+using AIronChef.Application.Users.Commands.DeleteUser;
+using AIronChef.Domain.Interfaces;
+using AIronChef.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIronChef.Tests.UnitTests.ApplicationTests.UsersTest
+{
+    [TestFixture]
+    public class DeleteUserHandlerTests
+    {
+        private IUserRepository _userRepository;
+        private ILoggingService _loggingService;
+        private DeleteUserCommandHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _userRepository = A.Fake<IUserRepository>();
+            _loggingService = A.Fake<ILoggingService>();
+            _handler = new DeleteUserCommandHandler(_userRepository, _loggingService);
+        }
+
+
+        [Test]
+        public async Task Handle_Should_ReturnSuccess_When_UserIsDeleted()
+        {
+            int userId = 1;
+            A.CallTo(() => _userRepository.DeleteUserAsync(userId))
+                .Returns(Task.FromResult(true));
+
+            var command = new DeleteUserCommand { Id = userId };
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            Assert.That(result.Success, Is.True);
+            Assert.That(result.ErrorMessage, Is.Null);
+        }
+
+        [Test]
+        public async Task Handle_Should_ReturnFailure_When_UserDoesNotExist()
+        {
+            int userId = 1999;
+            A.CallTo(() => _userRepository.DeleteUserAsync(userId))
+                .Returns(Task.FromResult<User>(null));
+
+            var command = new DeleteUserCommand { Id = userId };
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            Assert.That(result.Success, Is.False);
+            Assert.That(result.ErrorMessage, Is.EqualTo($"User with ID {userId} does not exist."));
+        }
+
+        [Test]
+        public async Task Handle_Should_ReturnFailure_When_IdIsInvalid()
+        {
+            int invalidId = -1;
+
+            var command = new DeleteUserCommand {Id = invalidId};
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            Assert.That(result.Success, Is.False);
+            Assert.That(result.ErrorMessage, Is.EqualTo("Invalid command: User ID is not valid."));
+        }
+
+        [Test]
+        public async Task Handle_Should_ReturnFailure_When_DatabaseErrorOccurs()
+        {
+            int userId = 1;
+            A.CallTo(() => _userRepository.DeleteUserAsync(userId))
+                .Throws(new Exception("Database error"));
+
+            var command = new DeleteUserCommand { Id = userId };
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            Assert.That(result.Success, Is.False);
+            Assert.That(result.ErrorMessage, Is.EqualTo("An error occurred while trying to delete the user."));
+        }
+    }
+}
