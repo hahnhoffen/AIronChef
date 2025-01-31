e27b5ccaa873d7b5494b49257b285c029b776378
KungSimon <simon.naslund@hotmail.com>
2025-01-31 09:32:02 +0100

    Added the GetRecipeHandler and Refactored GetRecipeQuery

--- AIronChef.Application/Recipes/Queries/GetRecipe/GetRecipeQuery.cs
+++ AIronChef.Application/Recipes/Queries/GetRecipe/GetRecipeQuery.cs
@@ -1,10 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AIronChef.Application.Common.Helpers;
+using AIronChef.Domain.Models;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace AIronChef.Application.Recipes.Queries.GetRecipe
 {
-    public class GetRecipeQuery
-    {
-        [Range(1, int.MaxValue, ErrorMessage = "Id must be greater than zero")]
-        public int Id { get; set; }
-    }
+    public record GetRecipeQuery (int Id) : IRequest<OperationResult<Recipe>>;
+    
 }
