5be90efadd0bb5e4aff390d10d8359288cc631ce
Mohammed Bekler <mohammed@kravia.ai>
2025-02-01 00:14:38 +0100

    changed GenerateRecipeCommandHandler

--- AIronChef.Application/Recipes/Commands/GenerateRecipe/GenerateRecipeCommand.cs
+++ AIronChef.Application/Recipes/Commands/GenerateRecipe/GenerateRecipeCommand.cs
@@ -1,5 +1,6 @@
 using AIronChef.Application.Common.Helpers;
 using AIronChef.Domain.Models;
+using AIronChef.Domain.Enums;
 using MediatR;
 
 namespace AIronChef.Application.Recipes.Commands.GenerateRecipe;
@@ -7,10 +8,10 @@ namespace AIronChef.Application.Recipes.Commands.GenerateRecipe;
 public class GenerateRecipeCommand : IRequest<OperationResult<Recipe>>
 {
     public string[] Ingredients { get; set; }
-    public int MaxCookingTimeMinutes { get; set; }
-    public Enum MealType { get; set; }
+    public int? MaxCookingTimeMinutes { get; set; }
+    public MealType MealType { get; set; }
     
-    public GenerateRecipeCommand(string[] ingredients, int maxCookingTimeMinutes, Enum mealType)
+    public GenerateRecipeCommand(string[] ingredients, int? maxCookingTimeMinutes, MealType mealType)
     {
         Ingredients = ingredients;
         MaxCookingTimeMinutes = maxCookingTimeMinutes;
