03201508b94a0727eb4a8d7837adc62a4cd12c64
Simon Näslund <143598052+KungSimon@users.noreply.github.com>
2025-02-01 19:40:11 +0100

    Update GetUserHandlerTests.cs
    Changed

--- AIronChef.Tests/UnitTests/ApplicationTests/UsersTest/GetUserHandlerTests.cs
+++ AIronChef.Tests/UnitTests/ApplicationTests/UsersTest/GetUserHandlerTests.cs
@@ -45,7 +45,7 @@ namespace AIronChef.Tests.UnitTests.ApplicationTests.UsersTest
         public async Task Handle_NonExistentUser_ReturnsFailure()
         {
             int invalidId = -1;
-            A.CallTo(() => _userRepository.GetByIdAsync(invalidId)).Returns(Task.FromResult<User>(null));
+            A.CallTo(() => _userRepository.GetByIdAsync(invalidId)).Returns(null);
 
             var query = new GetUserQuery(invalidId);
 
